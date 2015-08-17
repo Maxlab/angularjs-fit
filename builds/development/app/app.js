a6890d2bb5dccae60cd334182090fc75d6196f32
sunway_os <forsag1982@yandex.ru>
2015-08-17 16:50:25 +0300

    update

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -2,17 +2,32 @@
 $.material.init();
 
 angular.module('ngFit', ['ngRoute'])
-  .config(function($routeProvider){
-    $routeProvider
-      .when('/',{
-        templateUrl: '/view/main.html'
-      })
-      .when('/contact',{
-        templateUrl: '/view/contact.html'
-      })
-      .when('/about',{
-        templateUrl: '/view/about.html'
-      });
+  .config(ngFitConfig)
+  .controller('MainCtrl', MainCtrl)
+  .controller('ContactCtrl',ContactCtrl);
 
-  })
-;
+function MainCtrl($scope) {
+  $scope.title = 'Main page - contrler';
+}
+
+function ContactCtrl($scope) {
+  $scope.title = 'Contact - My name is Nikolay'
+  $scope.sayHello = function() {
+    alert('Hi, Nikolay!');
+  }
+}
+
+function ngFitConfig($routeProvider) {
+  $routeProvider
+    .when('/',{
+      templateUrl: '/view/main.html',
+      controller: 'MainCtrl'
+    })
+    .when('/contact',{
+      templateUrl: '/view/contact.html',
+      controller: 'ContactCtrl'
+    })
+    .when('/about',{
+      templateUrl: '/view/about.html'
+    });
+}
