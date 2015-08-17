9d0c529da4ee8d04f69ce4d05b2a03d891a5cb38
sunway_os <forsag1982@yandex.ru>
2015-08-17 13:51:30 +0300

    update

--- builds/development/app/app.js
+++ builds/development/app/app.js
@@ -1,4 +1,18 @@
 // initialize material design js
 $.material.init();
 
-angular.module('ngFit', []);
+angular.module('ngFit', ['ngRoute'])
+  .config(function($routeProvider){
+    $routeProvider
+      .when('/',{
+        templateUrl: '/view/main.html'
+      })
+      .when('/contact',{
+        templateUrl: '/view/contact.html'
+      })
+      .when('/about',{
+        templateUrl: '/view/about.html'
+      });
+
+  })
+;
